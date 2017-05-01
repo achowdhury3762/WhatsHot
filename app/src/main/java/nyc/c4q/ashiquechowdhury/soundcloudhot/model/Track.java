c1056c2da386986efb1304555d7a314aa8d2113f
achowdhury3762 <achowdhury3762@gmail.com>
2017-05-01 04:24:22 -0400

    Implemented Comparable in Track to sort using the following algorithm - PlaybackCount/ Follower Count, sorting happens in service layer hotmodel

--- app/src/main/java/nyc/c4q/ashiquechowdhury/soundcloudhot/model/Track.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/soundcloudhot/model/Track.java
@@ -1,11 +1,15 @@
 package nyc.c4q.ashiquechowdhury.soundcloudhot.model;
 
-public class Track {
+import android.support.annotation.NonNull;
+
+public class Track implements Comparable<Track> {
     public int playback_count;
 
     public String title;
 
     public String artwork_url;
+
+    public int favoritings_count;
 
     public String getTitle() {
         return title;
@@ -18,4 +22,22 @@ public class Track {
     public int getPlaybackCount(){
         return playback_count;
     }
+
+    public int getFavoritings_count() { return favoritings_count; }
+
+    @Override
+    public int compareTo(@NonNull Track track) {
+        float thisRatioOfPlaybackToFavorites = this.getPlaybackCount()/ this.getFavoritings_count();
+        float otherRatioOfPlaybackToFavorites = track.getPlaybackCount()/this.getFavoritings_count();
+
+        if(thisRatioOfPlaybackToFavorites < otherRatioOfPlaybackToFavorites) {
+            return 1;
+        }
+        if(thisRatioOfPlaybackToFavorites > otherRatioOfPlaybackToFavorites) {
+            return -1;
+        }
+        else
+            return 0;
+    }
+
 }
